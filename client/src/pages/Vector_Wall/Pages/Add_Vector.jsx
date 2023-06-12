986f565165d296316dc6d6bfc7d3246d101a67dc
fahim32446 <azmirhasan.az2@gmail.com>
2023-06-12 21:20:14 +0600

    fix image design

--- client/src/pages/Vector_Wall/Pages/Add_Vector.jsx
+++ client/src/pages/Vector_Wall/Pages/Add_Vector.jsx
@@ -3,12 +3,15 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Add_Vector = () => {
+  const [images, setImages] = useState([]);
   const [formData, setFormData] = useState({
     title: '',
     desc: '',
     price: '',
-    covers: [], // Change to an array
+    covers: [],
   });
+
+  console.log({ images });
 
   const navigate = useNavigate();
 
@@ -21,8 +24,12 @@ const Add_Vector = () => {
   };
 
   const handlePhotoUpload = (event) => {
+    const filesArr = Array.from(event.target.files);
     const files = event.target.files;
     setFormData((prevState) => ({ ...prevState, covers: [...files] }));
+
+    const uploadedImages = filesArr.map((file) => URL.createObjectURL(file));
+    setImages((prevImages) => [...prevImages, ...uploadedImages]);
   };
 
   const handleSubmit = async (event) => {
@@ -51,79 +58,82 @@ const Add_Vector = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto my-8">
-      <h1 className="text-center text-3xl font-bold mb-4">Add New Book</h1>
-      <form
-        encType="multipart/form-data"
-        onSubmit={handleSubmit}
-        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-      >
-        <div className="mb-4">
-          <label htmlFor="title" className="block text-gray-700 font-bold mb-2">
-            Title
-          </label>
-          <input
-            type="text"
-            name="title"
-            value={formData.title}
-            onChange={handleChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Enter title"
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="desc" className="block text-gray-700 font-bold mb-2">
-            Description
-          </label>
-          <textarea
-            name="desc"
-            value={formData.desc}
-            onChange={handleChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Enter description"
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="price" className="block text-gray-700 font-bold mb-2">
-            Price
-          </label>
-          <input
-            type="number"
-            name="price"
-            value={formData.price}
-            onChange={handleChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Enter price"
-          />
-        </div>
+    <div className=" mx-auto my-8">
+      <h1 className="text-center text-xl font-bold shadow mb-5 py-3 rounded">
+        Add New Vector Images
+      </h1>
+      <form encType="multipart/form-data" onSubmit={handleSubmit} className="">
+        <div className="flex gap-5">
+          <div className="flex-1 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+              <label
+                htmlFor="title"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Title
+              </label>
+              <input
+                type="text"
+                name="title"
+                value={formData.title}
+                onChange={handleChange}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="Enter title"
+              />
+            </div>
 
-        <div className="mb-4">
-          <label htmlFor="cover" className="block text-gray-700 font-bold mb-2">
-            Cover Photo
-          </label>
+            <div className="mb-4">
+              <label
+                htmlFor="cover"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Cover Photo
+              </label>
 
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="cover"
-            type="file"
-            name="covers"
-            accept="image/*"
-            multiple
-            onChange={handlePhotoUpload}
-          />
-        </div>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="cover"
+                type="file"
+                name="covers"
+                accept="image/*"
+                multiple
+                onChange={handlePhotoUpload}
+              />
+            </div>
 
-        <div className="flex items-center justify-center">
-          <button
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          >
-            Add Book
-          </button>
+            <div className="flex items-center justify-center">
+              <button
+                type="submit"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              >
+                Add Image
+              </button>
+            </div>
+          </div>
+
+          <div className="flex-1 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <label className="block font-bold mb-1">Preview</label>
+            <div className="grid grid-cols-4 gap-4 shadow">
+              {images.length > 0 ? (
+                images.map((image, index) => (
+                  <img
+                    key={index}
+                    src={image}
+                    alt={`Uploaded ${index}`}
+                    className="w-[100px] h-[200px] object-cover rounded shadow"
+                  />
+                ))
+              ) : (
+                <div className="border border-gray-300 p-2 h-48 flex items-center justify-center">
+                  No images selected
+                </div>
+              )}
+            </div>
+          </div>
         </div>
       </form>
     </div>
   );
 };
 
-export default Add_Vector
+export default Add_Vector;
